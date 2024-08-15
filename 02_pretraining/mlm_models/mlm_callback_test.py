89dfee9333b1d75336dfeb6fce40ddddcf74f9e8
Lukas Bierling <bierling.lukas@gmail.com>
2024-08-15 20:11:53 +0000

    added callback option, saving hf-dataset and mlm dataset init options

--- 02_pretraining/mlm_models/mlm_callback_test.py
+++
@@ -1,8 +0,0 @@
-from finlm.models import PretrainMLM
-from finlm.config import FinLMConfig
-from finlm.callbacks import CallbackManager
-
-config = FinLMConfig.from_yaml("mlm_config.yaml", "/data/language_models/pretrained_models/")
-
-mlm_modeling = PretrainMLM(config)
-mlm_modeling.train()
