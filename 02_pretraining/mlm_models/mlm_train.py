3ccdb001547988833e5d0a0f3be1c19d4b925207
Lukas Bierling <98786106+Coluding@users.noreply.github.com>
2025-04-09 10:58:11 +0200

    Feature/flexible encoder model (#10)
    * starting to include multiple models
    
    * added some large sequence models
    
    * added longformer and modelbert training
    
    * added longformer and modelbert training
    
    * fixed grad bug with longformer
    
    * Delete finlm/finlm/__pycache__ directory
    
    ---------
    
    Co-authored-by: Ralf Kellner <35298613+RalfKellner@users.noreply.github.com>

--- 02_pretraining/mlm_models/mlm_train.py
+++ 02_pretraining/mlm_models/mlm_train.py
@@ -1,8 +1,8 @@
-from finlm.models import PretrainMLM
+from finlm.models import PretrainMLM, PretrainMLM2
 from finlm.config import FinLMConfig
 from finlm.callbacks import CallbackManager
 
 config = FinLMConfig.from_yaml("mlm_config.yaml", "/data/language_models/pretrained_models/")
 
-mlm_modeling = PretrainMLM(config)
+mlm_modeling = PretrainMLM2(config)
 mlm_modeling.train()
